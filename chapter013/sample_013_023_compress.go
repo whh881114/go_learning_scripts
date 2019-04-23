c8d5396b80c6e6c814fc9814e462a1c5bbaf874f
Roy Wong <whh881114@gmail.com>
2019-04-23 21:32:36 +0800

    compress示例程序。

---
+++ chapter013/sample_013_023_compress.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"compress/gzip"
+	"log"
+	"os"
+)
+
+func main() {
+	outputFile, err := os.Create("test3.txt.gz")
+	if err != nil {
+		log.Fatal(err)
+	}
+	gzipWriter := gzip.NewWriter(outputFile)
+	defer gzipWriter.Close()
+
+	// 当写入gzip writer数据时，分它依次压缩数据并写入底层的文件中
+	// 不必关心它是如何压缩的，还是像普通的writer一样操作即可。
+	_, err = gzipWriter.Write([]byte("Gophers rule!\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("已经压缩数据并写入文件。")
+}
