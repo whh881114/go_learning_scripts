17f4e5d5f8ffa3a2dbe9c98e3cff2e8ed1150509
Roy Wong <whh881114@gmail.com>
2019-04-24 10:05:21 +0800

    https服务，使用自签名证书还是不行。

---
+++ chapter014/sample_014_003_https.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const SERVER_PORT = 8080
+const SERVER_DOMAIN = "localhost"
+const RESPONSE_TEMPLATE = "Hello, world."
+
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", fmt.Sprint(len(RESPONSE_TEMPLATE)))
+	w.Write([]byte(RESPONSE_TEMPLATE))
+}
+
+func main() {
+	http.HandleFunc(fmt.Sprintf("%s:%d/", SERVER_DOMAIN, SERVER_PORT), rootHandler)
+	http.ListenAndServeTLS(fmt.Sprintf(":%d", SERVER_PORT),
+		"/Users/Kanon/go/src/goTraining/chapter014/ssl.crt",
+		"/Users/Kanon/go/src/goTraining/chapter014/ssl.key",
+		nil,
+	)
+}
+
+// 自生成证书
+// 1. 生成密钥：openssl genrsa -out ssl.key 2048
+// 2. 根据密钥生成证书请求：openssl req -new -key ssl.key -out ssl.csr
+// 3. 自签名，生成私有证书：openssl x509 -req -in ssl.csr -signkey ssl.key -out ssl.crt
