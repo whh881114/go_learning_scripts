b637d64a7f00788b0a93876cea978590e866eebb
Roy Wong <whh881114@gmail.com>
2021-02-10 11:42:00 +0800

    添加注释

--- Roy/lesson006/waitgroup/main.go
+++ Roy/lesson006/waitgroup/main.go
@@ -9,12 +9,12 @@ var wg sync.WaitGroup
 
 func hello() {
 	fmt.Println("Hello world!")
-	wg.Done()
+	wg.Done() // 计数器-1
 }
 
 func main() {
-	go hello()
-	wg.Add(1) // 1. 创建一个goroutine；2. 在新的goroutine中执行hello函数。
+	go hello() // 1. 创建一个goroutine；2. 在新的goroutine中执行hello函数。
+	wg.Add(1)  // 计数器+1，根据协程的数量设置。
 	fmt.Println("Hello main func.")
 	wg.Wait() // 阻塞，一直等待所有的goroutine函数执行完。
 }
