2505870e3c81646477cba6033c9b08f8cdb8ce2b
Roy Wong <whh881114@gmail.com>
2021-03-14 16:42:04 +0800

    将配置文件改为yaml格式

--- logdel/main.go
+++ logdel/main.go
@@ -1,90 +1,40 @@
 package main
 
+// 程序名：logdel
+// 用途：删除带有日期文件，特别是java应用产生的日志，例如tomcat应用日志。
+
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	log "github.com/sirupsen/logrus"
-	"io"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
 var optConfDir string
 var optDryRun bool
-var optDate string
 
-var logFiles []*logFile
-
-type logFile struct {
-	LogFilePattern string
-	RemainedDays   int
-	LogFiles       []string
+type Configurations struct {
+	Global Global  `yaml:"global"`
+	Items  []Items `yaml:"items"`
 }
 
-func (logfile *logFile) getLogFiles() {
-	// 支持日志文件中日期格式：":", "-", "."和"_"
-	var logFileSeps = []string{":", "-", ".", "_"}
-	logfile.LogFiles = make([]string, 0)
-	// wildcardMaxNum还需要加1是因为最后的日志文件中包含中一个"*"，来达到支持三层目录通配。
-	wildcardMaxNum := 3
-
-	for _, logFileSep := range logFileSeps {
-		// 支持目录通配符，最大支持三层目录通配，如/tmp/*/*/*/*.log，第一层目录不能为"*"。
-		wildcardNum := strings.Count(logfile.LogFilePattern, "/*/")
-		if wildcardNum > wildcardMaxNum {
-			log.Warnf("配置项中，支持目录通配符，最大支持三层目录通配，如/tmp/*/*/*/*.log，不支持多个*。")
-			return
-		}
-
-		if strings.Index(logfile.LogFilePattern, "/*/") == 0 {
-			log.Warnf("配置项中，第一层目录不能为\"*\"。")
-			return
-		}
-
-		logFileList := strings.Split(logfile.LogFilePattern, "/")
-		logFileList = logFileList[1:] // 去掉最前面的空字符串
-		logFileFlag := logFileList[len(logFileList)-1]
-		if !strings.Contains(logFileFlag, "*") {
-			log.Warnf("配置项中，最后日志文件中没有通配符\"*\"，这个是不可以的，因为当前不能根据给出的目录删除何种日志文件。")
-			return
-		}
-		var topLogFileDir string
-		topLogFileDir += "/" + logFileList[0]
-		i := 0
-	LOOP1:
-		dirs, _ := ioutil.ReadDir(topLogFileDir)
-		for _, dir := range dirs {
-			if dir.IsDir() {
-				i++
-				topLogFileDir += "/" + dir.Name()
-				fmt.Printf("LOOP1: %v\n", topLogFileDir)
-				if i != wildcardNum {
-					goto LOOP1
-				}
-				goto LOOP2
-			}
-		}
-	LOOP2:
-		fmt.Printf("LOOP2: %v\n", topLogFileDir)
-		dirs2, _ := ioutil.ReadDir(topLogFileDir)
-		for _, dir := range dirs2 {
-			if dir.IsDir() {
-				logfile.LogFiles = append(logfile.LogFiles, dir.Name())
-			}
-		}
-		fmt.Println(logFileSep)
-	}
+type Global struct {
+	RemainedNum int      `yaml:"remained_num"`
+	DateFormats []string `yaml:"date_formats"`
+	Suffixes    []string `yaml:"suffixes"`
 }
 
-func (logfile *logFile) delLogFiles() {
-
+type Items struct {
+	Name        string   `yaml:"name"`
+	Paths       []string `yaml:"paths"`
+	RemainedNum int      `yaml:"remained_num"`
+	DateFormats []string `yaml:"date_formats"`
+	Suffixes    []string `yaml:"suffixes"`
 }
 
-// getConfFiles 根据--conf-dir参数给定的配置目录，获取到yaml配置文件列表。
 func getConfFiles(confDir string) []string {
 	confFiles := make([]string, 0)
 	files, err := ioutil.ReadDir(confDir)
@@ -95,8 +45,8 @@ func getConfFiles(confDir string) []string {
 	for _, file := range files {
 		if !file.IsDir() {
 			fileName := file.Name()
-			// 根据文件后缀".conf"检查是否为配置文件。
-			if strings.HasSuffix(fileName, ".conf") {
+			// 配置文件后缀名为`.yaml`或`.yml`。
+			if strings.HasSuffix(fileName, ".yaml") || strings.HasSuffix(fileName, ".yml") {
 				confFiles = append(confFiles, optConfDir+"/"+file.Name())
 			}
 		}
@@ -104,74 +54,120 @@ func getConfFiles(confDir string) []string {
 	return confFiles
 }
 
-// 过滤掉以#开头的注释。
-func getConfItem(str string) {
-	reg, _ := regexp.Compile("^(/.*(/.*)*):\\s+(\\d+)\\s*$")
-	regStrings := reg.FindAllSubmatch([]byte(str), -1)
+func decodeConfFile(confFile string) (*Configurations, error) {
+	conf := new(Configurations)
 
-	for _, str := range regStrings {
-		myLogFile := new(logFile)
-		myLogFile.LogFilePattern = string(str[1])
-		myLogFile.RemainedDays, _ = strconv.Atoi(string(str[3]))
-		logFiles = append(logFiles, myLogFile)
-	}
-}
-
-// parseConfFile 解析yaml配置文件
-func parseConfFile(confFile string) {
 	f, err := os.Open(confFile)
 	if err != nil {
-		log.Warnf("打开配置文件目录\"%s\"失败。", confFile)
+		log.Errorf("打开配置文件目录\"%s\"失败。\n", confFile)
+		return conf, err
 	}
 	defer f.Close()
 
-	reader := bufio.NewReader(f)
-	for {
-		str, err := reader.ReadString('\n')
-		if err == io.EOF {
-			getConfItem(str)
-			return
-		}
-		if err != nil {
-			log.Warnf("读取配置项\"%s\"失败。具体报错：%v\n", str, err)
-		}
-		getConfItem(str)
+	if err := yaml.NewDecoder(f).Decode(conf); err != nil {
+		return conf, err
 	}
+
+	return conf, nil
+}
+
+func parseConf(confFile string, confData *Configurations) []Items {
+	instances := make([]Items, 0)
+
+	// 当全局参数没有配置时，使用默认值。
+	defaultRemainedNum := 7
+	defaultDateFormats := []string{"YYYY-MM-DD", "YYYYMMDD", "YYYY_MM_DD"}
+	defaultSuffixes := []string{".log", ".txt"}
+
+	// 当全局参数配置时，覆盖默认值。
+	if confData.Global.RemainedNum != 0 {
+		defaultRemainedNum = confData.Global.RemainedNum
+	}
+
+	if len(confData.Global.DateFormats) != 0 {
+		defaultDateFormats = confData.Global.DateFormats
+	}
+
+	if len(confData.Global.Suffixes) != 0 {
+		defaultSuffixes = confData.Global.Suffixes
+	}
+
+	// 当配置文件中，没有填写items配置时，其中name和path均为默认值，则过滤此配置。
+	for _, item := range confData.Items {
+		if len(item.Name) == 0 {
+			log.Warnf("配置文件\"%s\"配置项items子项目\"%#v\"中\"name\"参数为空，忽略此配置项。", confFile, item)
+			continue
+		}
+
+		if len(item.Paths) == 0 {
+			log.Warnf("配置文件\"%s\"配置项items子项目\"%#v\"中\"paths\"参数为空，忽略此配置项。", confFile, item)
+			continue
+		}
+
+		if item.RemainedNum == 0 {
+			item.RemainedNum = defaultRemainedNum
+		}
+
+		if len(item.DateFormats) == 0 {
+			item.DateFormats = defaultDateFormats
+		}
+
+		if len(item.Suffixes) == 0 {
+			item.Suffixes = defaultSuffixes
+		}
+
+		instances = append(instances, item)
+	}
+	return instances
+}
+
+func getLogFiles(logItems []Items) []string {
+	logFiles := make([]string, 0)
+	return logFiles
+
+}
+
+func delLogFiles(logFiles []string) {
+	fmt.Println("删除文件")
 }
 
 func main() {
 	// 获取参数。
-	// 默认配置文件目录在/etc/logdel.d，配置文件以.conf结尾，不能放在这个目录下其他目录下。
+	// 默认配置文件目录在/etc/logdel.d，配置文件以`.yaml`或`.yml`结尾，不能放在这个目录下其他目录下。
 	flag.StringVar(&optConfDir, "conf-dir", "/etc/logdel.d", "Set configuration directory.")
 
-	// 运行时，默认跑在dry-run下，并且日志打印在终端，如果不在此模式下就就印在/var/log/logdel.log下，并且日志按100M分割大小，
-	// 保留7个副本，不压缩，日志内容格式为json格式。
+	// 运行时，默认跑在dry-run下，并且日志打印在终端。
 	flag.BoolVar(&optDryRun, "dry-run", true, "Run in dry-run mode.")
 
-	// 指定日期，用于调试，并且日志打印在终端。
-	flag.StringVar(&optDate, "date", "", "Set date (in format of YYYY-MM-DD) for debugging.")
 	flag.Parse()
 
 	// 获取配置目录下的配置文件。
 	confFiles := getConfFiles(optConfDir)
 
 	if len(confFiles) == 0 {
-		log.Warnf("配置文件目录\"%s\"下无配置文件。", optConfDir)
+		log.Errorf("配置文件目录\"%s\"下无配置文件。", optConfDir)
 		return
 	} else {
-		log.Infof("配置文件目录\"%s\"下配置文件列表：%#v。\n", optConfDir, confFiles)
+		log.Infof("配置文件目录\"%s\"下配置文件列表：%v。\n", optConfDir, confFiles)
 	}
 
-	// 解析配置文件，配置文件默认存放在/etc/logdel.d中，文件是yaml格式。
 	for _, confFile := range confFiles {
-		parseConfFile(confFile)
-	}
+		if confData, err := decodeConfFile(confFile); err != nil {
+			log.Errorf("解析配置文件\"%s\"失败，跳过此配置文件。\n", confFile)
+		} else {
+			log.Infof("解析配置文件\"%s\"成功。\n", confFile)
+			logItems := parseConf(confFile, confData)
 
-	// 根据获取到的每个配置项，获取到要删除的文件列表。
-	for _, item := range logFiles {
-		item.getLogFiles()
-		fmt.Println(item.LogFiles)
-	}
+			if len(logItems) == 0 {
+				log.Warnf("配置文件\"%s\"中无items配置项。\n", confFile)
+			} else {
+				logFiles := getLogFiles(logItems)
 
-	// 根据文件列表删除文件。
+				// 当dry-run为false时，就执行删除文件。
+				if !optDryRun {
+					delLogFiles(logFiles)
+				}
+			}
+		}
+	}
 }
